15181a0c27c16532fdecceaa30c689e8775f4604
Ulil Albab Surya Negara <surya.negara@gontor.ac.id>
2021-12-19 10:54:02 +0100

    get available agents 1.1

--- agent-allocation-service.ts
+++ agent-allocation-service.ts
@@ -1,9 +1,29 @@
-const GET_AVAILABLE_AGENT = async () => {
+import axios from 'axios';
+import { createClient } from '@supabase/supabase-js'
 
-}
-
-const GET_UNSERVED_CLIENT = async () => {
-
-}
+const supabaseUrl = 'https://ecsxaprxkvyoolvvobsn.supabase.co'
+const supabaseKey = process.env.SUPABASE_KEY
+const supabase = createClient(supabaseUrl, supabaseKey)
 
 const QISCUS_APP_ID = process.env.QISCUS_APP_ID;
+const QISCUS_APP_SECRET = process.env.QISCUS_SECRET_KEY;
+const BASE_URL = process.env.BASE_URL;
+
+const GET_AVAILABLE_AGENTS = async () => {
+  let available_agents = await axios.get('https://multichannel.qiscus.com/api/v1/admin/agents', {
+      headers: {
+          'Qiscus-App-Id': process.env.QISCUS_APP_ID,
+          'Qiscus-Secret-Key': process.env.QISCUS_SECRET_KEY
+      }
+  }).then((response) => {
+      return response.data.data.agents.data;
+  })
+  available_agents = available_agents.filter(agent => agent.is_available && agent.current_customer_count < 2 )
+}
+
+const ASSIGN_AGENT = async () => {
+
+}
+
+const res = await GET_AVAILABLE_AGENTS();
+return res;
