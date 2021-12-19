fa7827ac1f11769d8415959442d11ddc27751df9
Ulil Albab Surya Negara <surya.negara@gontor.ac.id>
2021-12-19 11:31:10 +0100

    add unserved customer to waiting list while no available agent

--- agent-allocation-service.ts
+++ agent-allocation-service.ts
@@ -22,11 +22,13 @@ const GET_AVAILABLE_AGENTS = async () => {
   available_agents = available_agents.filter(agent => agent.is_available && agent.current_customer_count < 2 )
   if (available_agents.length > 0) {
     await ASSIGN_AGENT(available_agents[0].id, steps.trigger.event.body.room_id)
+  } else {
+    await ADD_TO_WAITING_LIST(steps.trigger.event.body.room_id)
   }
 }
 
 const ASSIGN_AGENT = async (agentId, roomId) => {
-  let assign_agent = await axios.post('https://multichannel.qiscus.com/api/v1/admin/service/assign_agent',
+  await axios.post('https://multichannel.qiscus.com/api/v1/admin/service/assign_agent',
   querystring.stringify({
       agent_id: agentId,
       room_id: roomId,
@@ -37,23 +39,21 @@ const ASSIGN_AGENT = async (agentId, roomId) => {
           'Qiscus-Secret-Key': process.env.QISCUS_SECRET_KEY,
       }
   })
-  .then(response => response);
-  console.log(assign_agent);
+  .catch((err) => err);
 }
 
 const ADD_TO_WAITING_LIST = async (roomId) => {
-    const isWaiting = await supabase
+  const isWaiting = await supabase
+    .from('queues')
+    .select('*')
+    .match({room_id: roomId}).length === 1;
+  if (!isWaiting) {
+    const {data: addToWaitingList} = await supabase
       .from('queues')
-      .select('*')
-      .match({room_id: roomId}).length === 1;
-    if (!isWaiting) {
-      const {data: addToWaitingList} = await supabase
-        .from('queues')
-        .insert([{
-          room_id: roomId
-        }])
-    }
+      .insert([{
+        room_id: roomId,
+        is_resolved: false,
+      }])
   }
-
-const res = await GET_AVAILABLE_AGENTS();
-return res;
+}
+await GET_AVAILABLE_AGENTS();
